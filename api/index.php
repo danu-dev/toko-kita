e6c06920977ed7a51abd235a8a84595284903ce7
danu-dev <arsab201@gmail.com>
2026-08-22 12:38:08 +0700

    fix: route logging to php://stderr on Vercel

--- api/index.php
+++ api/index.php
@@ -1,33 +1,36 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
+// Direct serverless env mappings for Vercel
+putenv('APP_ENV=production');
+putenv('APP_DEBUG=false');
+putenv('LOG_CHANNEL=stderr');
+putenv('VIEW_COMPILED_PATH=/tmp/views');
+putenv('DB_CONNECTION=sqlite');
+putenv('DB_DATABASE=/tmp/database.sqlite');
+putenv('SESSION_DRIVER=cookie');
+putenv('CACHE_STORE=array');
 
-// Set up serverless writable tmp directories
-$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
-$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
-$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
-$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
-$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_ENV'] = 'production';
+$_ENV['APP_DEBUG'] = 'false';
+$_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
-$_ENV['LOG_CHANNEL'] = 'errorlog';
-$_ENV['SESSION_DRIVER'] = 'cookie';
-$_ENV['CACHE_STORE'] = 'array';
 $_ENV['DB_CONNECTION'] = 'sqlite';
 $_ENV['DB_DATABASE'] = '/tmp/database.sqlite';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['CACHE_STORE'] = 'array';
 
-if (!file_exists('/tmp/views')) {
-    mkdir('/tmp/views', 0755, true);
+if (!is_dir('/tmp/views')) {
+    @mkdir('/tmp/views', 0755, true);
 }
 
-// Copy seed database to /tmp if it doesn't exist
 $sourceDb = __DIR__ . '/../database/database.sqlite';
 $targetDb = '/tmp/database.sqlite';
 
 if (!file_exists($targetDb)) {
     if (file_exists($sourceDb)) {
-        copy($sourceDb, $targetDb);
+        @copy($sourceDb, $targetDb);
     } else {
-        touch($targetDb);
+        @touch($targetDb);
     }
 }
 
